775d15cd38e994645ffff3853b8ab52a3b2289e7
amiraliansaripour <amiraliansaripor1383@gmail.com>
2024-09-16 15:55:44 +0330

    coin table

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Calculator = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="flex items-center justify-center w-[500px] h-[500px] bg-gradient-to-t from-orange-300 to-yellow-200 rounded-full">
+      <div className="flex items-center justify-center w-[500px] max-sm:w-[365px] h-[500px] max-md: bg-gradient-to-t from-orange-300 to-yellow-200 rounded-full">
         <div className="w-[450px] h-[450px] flex-col border-2 border-gray-300 bg-white rounded-lg overflow-hidden">
           <div className="w-full h-[60px] flex items-center justify-center bg-[#2E2A22] text-white">
             Currency Converter
--- src/components/coinMiniTable/CoinMiniTable.jsx
+++ src/components/coinMiniTable/CoinMiniTable.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const CoinMiniTable = () => {
   return (
-       
-    <div className='flex items-center justify-center'>
-    .ddddddddddddddd
-          </div>
-  )
-}
+    <div className="flex items-center max-lg:mt-10">
+      <div className="flex flex-col items-center justify-center w-[500px] max-sm:w-[365px] max-xl:max-w-2xl gap-3">
+        <h2 className="font-bold">Crypto Currency Pricing Table</h2>
+        <table className="w-full border-[1px] rounded-xl ">
+          <tr className="border-[1px]">
+            <td className="px-4 py-1">Name</td>
+            <td className="px-4 py-1">Price</td>
+            <td className="px-4 py-1">24h Change</td>
+          </tr>
+         {
+          ["","","","","",""].map((coin)=>{
+            return(
+              <tr className="border-[1px]">
+              <td className="px-4 py-3 flex items-center gap-2 cursor-pointer">
+                <div className="img-box w-8 h-8 rounded-full bg-main-yellow">
+                  <img src="https://logos-world.net/wp-content/uploads/2020/08/Bitcoin-Logo.png" className="w-full h-full object-cover" alt="" />
+                </div>
+                <span>BTC</span>
+              </td>
+              <td className="px-4 py-3">45.000 $</td>
+              <td className="px-4 py-3 text-green-500">+24 %</td>
+            </tr>
+            )
+          })
+         }
+         <tr className="border-[1px]">
+              <td className="px-4 py-3 flex items-center gap-2">
+                <span className="text-lg text-gray-400">220 more record</span>
+              </td>
+              <td className="px-4 py-3"></td>
+              <td className="px-4 py-3 text-right cursor-pointer">View all</td>
+            </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default CoinMiniTable
+export default CoinMiniTable;
