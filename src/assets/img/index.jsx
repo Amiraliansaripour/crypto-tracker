99b0ca6881278c4a3c7ae5c5d5525ba6470c2116
amiraliansaripour <amiraliansaripor1383@gmail.com>
2024-09-16 15:54:12 +0330

    export image

--- src/assets/img/index.jsx
+++ src/assets/img/index.jsx
@@ -1,3 +1,8 @@
 // hero section 
 export {default as HeroImage} from './hero.png'
 export {default as Logo} from './logo.png'
+export {default as CoinLaptop} from './coin-laptop.png'
+export {default as S1} from './s1.png'
+export {default as S2} from './s2.png'
+export {default as S3} from './s3.png'
+export {default as Laptop} from './laptop.png'
