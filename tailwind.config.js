a01c8e0fd7d5d578b2286f25abb2770305d3be17
amiraliansaripour <amiraliansaripor1383@gmail.com>
2024-09-16 15:53:47 +0330

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,25 @@ module.exports = {
         colors:{
           'main-gray':'#5F5F5F',
           'main-yellow':'#FFDF66'
-        }
+        },
+        container: {
+          center: true, 
+          padding: "1rem", 
+        },
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "960px",
+          xl: "1140px",
+          "2xl": "1320px",
+        },
+        maxWidth: {
+          sm: "100%", // or customize as per your design
+          md: "720px",
+          lg: "960px",
+          xl: "1140px",
+          "2xl": "1320px",
+        },
       },
     },
     plugins: [],
